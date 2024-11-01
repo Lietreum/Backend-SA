97f87df7f4fde4d6519ca3c9cfcd94a68246bdf5
Lietreum <mirsieolien@gmail.com>
2024-11-01 14:27:38 +0700

    a

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,35 @@
-// src/api/index.js
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import config from "./config/config.js";
+import router from "./router/router.js";
+
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("Hello from Express!");
+app.use(
+  cors({
+    origin: "https://smart-att.curaweda.com",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log("Request Method:", req.method);
+  console.log("Request Path:", req.path);
+  console.log("Content-Type:", req.get("Content-Type"));
+  console.log("Request Body (raw):", req.body);
+  next();
 });
 
-// Export the Express app to be used as a serverless function
-module.exports = app;
+app.use("/api", router);
+
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
+  res.status(500).json({ error: err.message || "Something went wrong" });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
